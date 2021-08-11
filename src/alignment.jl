73182028412706651b760c725d02e7c4b21c6095
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2021-08-11 22:36:36 +0100

    Implement intersect alignment

--- src/alignment.jl
+++ src/alignment.jl
@@ -51,6 +51,14 @@ function create_evaluation_state(
     )
 end
 
+function create_evaluation_state(
+    ::Tuple{Node, Node},
+    ::BinaryAlignedNodeOp{T, IntersectAlignment},
+) where {T}
+    # Intersect alignment doesn't require remembering any previous state.
+    return _EMPTY_NODE_STATE
+end
+
 function _equal_times(a::Block, b::Block)::Bool
     # FIXME This doesn't account for the case where e.g. a.times is a vector, and b.times is
     #   a view of the entirety of a.times.
@@ -58,13 +66,7 @@ function _equal_times(a::Block, b::Block)::Bool
 end
 
 """Apply, assuming `input_l` and `input_r` have identical alignment."""
-function _apply_fast_align_binary!(
-    T,
-    state::BinaryAlignmentState,
-    f,
-    input_l::Block,
-    input_r::Block,
-)
+function _apply_fast_align_binary(T, f, input_l::Block, input_r::Block)
     n = length(input_l)
     values = _allocate_values(T, n)
     # We shouldn't assume that it is valid to broadcast f over the inputs, so loop manually.
@@ -72,9 +74,6 @@ function _apply_fast_align_binary!(
         @inbounds values[i] = f(input_l.values[i], input_r.values[i])
     end
 
-    # Update the alignment state.
-    state.latest_l = last(input_l.values)
-    state.latest_r = last(input_r.values)
     return Block(input_l.times, values)
 end
 
@@ -103,11 +102,16 @@ function run_node!(
 
     if _equal_times(input_l, input_r)
         # Times are indistinguishable
-        return _apply_fast_align_binary!(T, state, f, input_l, input_r)
+        # Update the alignment state.
+        state.latest_l = last(input_l.values)
+        state.latest_r = last(input_r.values)
+        return _apply_fast_align_binary(T, f, input_l, input_r)
     end
 
     # Create our outputs as the maximum possible size.
-    max_size = length(input_l) + length(input_r)
+    nl = length(input_l)
+    nr = length(input_r)
+    max_size = nl + nr
     times = Vector{DateTime}(undef, max_size)
     values = _allocate_values(T, max_size)
 
@@ -123,32 +127,29 @@ function run_node!(
     next_time_l = DateTime(0)
     next_time_r = DateTime(0)
 
-    nl = length(input_l)
-    nr = length(input_r)
-
     # Loop until we exhaust inputs.
     while (il <= nl || ir <= nr)
         if (il <= nl)
-            next_time_l = input_l.times[il]
+            next_time_l = @inbounds input_l.times[il]
         end
         if (ir <= nr)
-            next_time_r = input_r.times[ir]
+            next_time_r = @inbounds input_r.times[ir]
         end
 
         new_time = if (il <= nl && next_time_l < next_time_r) || ir > nr
             # Left ticks next
-            state.latest_l = input_l.values[il]
+            state.latest_l = @inbounds input_l.values[il]
             il += 1
             next_time_l
         elseif (ir <= nr && next_time_r < next_time_l) || il > nl
             # Right ticks next
-            state.latest_r = input_r.values[ir]
+            state.latest_r = @inbounds input_r.values[ir]
             ir += 1
             next_time_r
         else
             # A shared time point where neither x1 nor x2 have been exhausted.
-            state.latest_l = input_l.values[il]
-            state.latest_r = input_r.values[ir]
+            state.latest_l = @inbounds input_l.values[il]
+            state.latest_r = @inbounds input_r.values[ir]
             il += 1
             ir += 1
             next_time_l
@@ -160,8 +161,8 @@ function run_node!(
         end
 
         # Output a knot.
-        times[j] = new_time
-        values[j] = f(state.latest_l, state.latest_r)
+        @inbounds times[j] = new_time
+        @inbounds values[j] = f(state.latest_l, state.latest_r)
         j += 1
     end
 
@@ -171,5 +172,71 @@ function run_node!(
     resize!(times, j - 1)
     resize!(values, j - 1)
 
+    return Block(times, values)
+end
+
+function run_node!(
+    ::EmptyNodeEvaluationState,
+    node_op::BinaryAlignedNodeOp{T, IntersectAlignment},
+    ::DateTime,  # time_start
+    ::DateTime,  # time_end
+    input_l::Block{L},
+    input_r::Block{R},
+) where {T, L, R}
+    if isempty(input_l) || isempty(input_r)
+        # Output will be empty unless both inputs have ticked.
+        return Block{T}()
+    end
+
+    # This is the binary operator that we will be applying to input values.
+    f = binary_operator(node_op)
+
+    if _equal_times(input_l, input_r)
+        # Times are indistinguishable.
+        return _apply_fast_align_binary(T, f, input_l, input_r)
+    end
+
+    # Create our outputs as the maximum possible size.
+    nl = length(input_l)
+    nr = length(input_r)
+    max_size = max(nl, nr)
+    times = Vector{DateTime}(undef, max_size)
+    values = _allocate_values(T, max_size)
+
+    # Store indices into the inputs. The index represents the next time point for
+    # consideration for each series.
+    il = 1
+    ir = 1
+
+    # Index into the output.
+    j = 1
+
+    # If we get to the end of either series, we know that we cannot add any more elements to
+    # the output.
+    while (il <= nl && ir <= nr)
+        # Obtain the *next available* times from each entity. We know that the current
+        # state, and last emitted, time is strictly less than either of these.
+        time_l = @inbounds input_l.times[il]
+        time_r = @inbounds input_r.times[ir]
+
+        if time_l < time_r
+            # Left ticks next; consider the next knot.
+            il += 1
+        elseif time_r < time_l
+            # Right ticks next; consider the next knot.
+            ir += 1
+        else  # time_l == time_r
+            # Shared time point, so emit a knot.
+            @inbounds times[j] = time_l
+            @inbounds values[j] = f(input_l.values[il], input_r.values[ir])
+            j += 1
+            il += 1
+            ir += 1
+        end
+    end
+
+    # Package the outputs into a block, resizing the outputs as necessary.
+    resize!(times, j - 1)
+    resize!(values, j - 1)
     return Block(times, values)
 end
