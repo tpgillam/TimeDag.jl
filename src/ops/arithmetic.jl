af941009b000d1fd447378d05fce3f877474ad24
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2021-09-07 00:51:22 +0100

    Refactor binary operators with metaprogramming

--- src/ops/arithmetic.jl
+++ src/ops/arithmetic.jl
@@ -6,60 +6,32 @@ Base.:-(x::Node) = obtain_node((x,), Negate{value_type(x)}())
 
 struct Exp{T} <: StatelessUnaryNodeOp{T, true} end
 operator(::Exp, x) = exp(x)
-# TODO Nicer way to encode the type promotion?
+# FIXME Use output_type
 Base.exp(x::Node) = obtain_node((x,), Exp{typeof(exp(one(value_type(x))))}())
 
 struct Log{T} <: StatelessUnaryNodeOp{T, true} end
 operator(::Log, x) = log(x)
-# TODO Nicer way to encode the type promotion?
+# FIXME Use output_type
 Base.log(x::Node) = obtain_node((x,), Log{typeof(log(one(value_type(x))))}())
 
 # Binary operators
+struct BinaryArithmeticOp{T, A, Op} <: BinaryAlignedNodeOp{T, A} end
+operator(::BinaryArithmeticOp{T, A, Op}, x, y) where {T, A, Op} = Op(x, y)
 
-struct Add{T, A} <: BinaryAlignedNodeOp{T, A} end
-operator(::Add, x, y) = x + y
-function add(x, y; alignment::Type{A}=DEFAULT_ALIGNMENT) where {A <: Alignment}
-    x = _ensure_node(x)
-    y = _ensure_node(y)
-    T = output_type(+, value_type(x), value_type(y))
-    return obtain_node((x, y), Add{T, A}())
-end
-Base.:+(x::Node, y::Node) = add(x, y)
-Base.:+(x::Node, y) = add(x, y)
-Base.:+(x, y::Node) = add(x, y)
+for (long, short) in [(:add, :+), (:subtract, :-), (:multiply, :*), (:divide, :/)]
+    alias_sym = Symbol(titlecase(string(long)))
+    @eval begin
+        const $alias_sym{T, A} = BinaryArithmeticOp{T, A, $short}
 
-struct Subtract{T, A} <: BinaryAlignedNodeOp{T, A} end
-operator(::Subtract, x, y) = x - y
-function subtract(x, y; alignment::Type{A}=DEFAULT_ALIGNMENT) where {A <: Alignment}
-    x = _ensure_node(x)
-    y = _ensure_node(y)
-    T = output_type(-, value_type(x), value_type(y))
-    return obtain_node((x, y), Subtract{T, A}())
-end
-Base.:-(x::Node, y::Node) = subtract(x, y)
-Base.:-(x::Node, y) = subtract(x, y)
-Base.:-(x, y::Node) = subtract(x, y)
+        function $long(x, y; alignment::Type{A}=DEFAULT_ALIGNMENT) where {A <: Alignment}
+            x = _ensure_node(x)
+            y = _ensure_node(y)
+            T = output_type($short, value_type(x), value_type(y))
+            return obtain_node((x, y), $alias_sym{T, A}())
+        end
 
-struct Multiply{T, A} <: BinaryAlignedNodeOp{T, A} end
-operator(::Multiply, x, y) = x * y
-function multiply(x, y; alignment::Type{A}=DEFAULT_ALIGNMENT) where {A <: Alignment}
-    x = _ensure_node(x)
-    y = _ensure_node(y)
-    T = output_type(*, value_type(x), value_type(y))
-    return obtain_node((x, y), Multiply{T, A}())
+        Base.$short(x::Node, y::Node) = $long(x, y)
+        Base.$short(x::Node, y) = $long(x, y)
+        Base.$short(x, y::Node) = $long(x, y)
+    end
 end
-Base.:*(x::Node, y::Node) = multiply(x, y)
-Base.:*(x::Node, y) = multiply(x, y)
-Base.:*(x, y::Node) = multiply(x, y)
-
-struct Divide{T, A} <: BinaryAlignedNodeOp{T, A} end
-operator(::Divide, x, y) = x / y
-function divide(x, y; alignment::Type{A}=DEFAULT_ALIGNMENT) where {A <: Alignment}
-    x = _ensure_node(x)
-    y = _ensure_node(y)
-    T = output_type(/, value_type(x), value_type(y))
-    return obtain_node((x, y), Divide{T, A}())
-end
-Base.:/(x::Node, y::Node) = divide(x, y)
-Base.:/(x::Node, y) = divide(x, y)
-Base.:/(x, y::Node) = divide(x, y)
