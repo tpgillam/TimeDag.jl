73cc51d64c942ed85ed8b89e9ea9cee48edcc696
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2021-10-03 19:05:56 +0100

    Fix problem with evaluation, where intermediate blocks would be kept around forever

--- src/evaluation.jl
+++ src/evaluation.jl
@@ -6,19 +6,19 @@ Some thoughts on requirements
     - should know which nodes we *care* about, for keeping outputs.
     - needs all ancestors & map to (mutable) states
     - should keep track of the current time.
-    - should be copyable (to save current state) TODO
     - should contain output blocks??
 """
 mutable struct EvaluationState
-    evaluation_order::Vector{Node}
+    ordered_node_to_children::OrderedDict{Node,Set{Node}}
     node_to_state::IdDict{Node,NodeEvaluationState}
     current_time::DateTime
+    # TODO Decouple this from the evaluation state?
     evaluated_node_to_blocks::IdDict{Node,Vector{Block}}
 end
 
 function duplicate_internal(x::EvaluationState, stackdict::IdDict)
     y = EvaluationState(
-        x.evaluation_order,
+        x.ordered_node_to_children,
         IdDict((
             node => duplicate_internal(state, stackdict) for
             (node, state) in x.node_to_state
@@ -33,8 +33,19 @@ end
 function start_at(nodes, time_start::DateTime)::EvaluationState
     # Create empty evaluation state for all these, and return in some suitable pacakge.
     evaluation_order = ancestors(nodes...)
+
+    ordered_node_to_children = OrderedDict{Node,Set{Node}}(
+        node => Set{Node}() for node in evaluation_order
+    )
+
+    for node in evaluation_order
+        for parent in parents(node)
+            push!(ordered_node_to_children[parent], node)
+        end
+    end
+
     return EvaluationState(
-        evaluation_order,
+        ordered_node_to_children,
         IdDict(map(node -> node => create_evaluation_state(node), evaluation_order)),
         time_start,
         IdDict((node => Block{value_type(node)}[] for node in nodes)),
@@ -47,29 +58,43 @@ end
 Update the evaluation state by performing the evalution for each node.
 """
 function get_up_to!(state::EvaluationState, time_end::DateTime)::EvaluationState
-    # FIXME This is really suboptimal, as we keep all inputs around even after we're done
-    #   with them. Can certainly do better with a more appropriate data structure.
-
     # TODO Could we use dagger here to solve this & parallelism for us? I think the problem
     #   with this could be mutation - needs thought.
+    #
+    #   Also, initial experiments suggest that Dagger has about 100x overhead for simple
+    #   graphs (about 30s for 10^5 nodes, compared to 0.3s for full evaluation of a simple
+    #   TimeDag graph in single-threaded mode).
+    #
+    #   A reasonable approach is likely to allow the user to specify a scheduler when
+    #   evaluating.
 
-    node_to_block = IdDict{Node,Block}()
+    node_to_input_blocks = Dict(
+        node => Vector{Block}(undef, length(parents(node)))
+        for node in keys(state.ordered_node_to_children)
+    )
 
-    for node in state.evaluation_order
+    for node in keys(state.ordered_node_to_children)
         node_state = state.node_to_state[node]
-        # Retrieve the input blocks for all parents.
-        input_blocks = [node_to_block[parent] for parent in parents(node)]
+
+        # Retrieve the input blocks for all parents, and discard the reference.
+        input_blocks = pop!(node_to_input_blocks, node)
 
         # Run the node.
         block = run_node!(
             node_state, node.op, state.current_time, time_end, input_blocks...
         )
-        node_to_block[node] = block
+        for child in state.ordered_node_to_children[node]
+            # Place the block in the location(s) that this child expects to find it.
+            for i_parent in findall(isequal(node), parents(child))
+                node_to_input_blocks[child][i_parent] = block
+            end
+        end
 
-        if haskey(state.evaluated_node_to_blocks, node)
+        evaluated_blocks = get(state.evaluated_node_to_blocks, node, nothing)
+        if !isnothing(evaluated_blocks)
             # The current node is of interest - persist its output onto the evaluation
             # state.
-            push!(state.evaluated_node_to_blocks[node], block)
+            push!(evaluated_blocks, block)
         end
     end
 
@@ -106,8 +131,7 @@ function evaluate(
             t < time_end || break
         end
     end
-    # TODO Is using splatting a performance overhead over a vector if there are many blocks?
-    return [vcat(state.evaluated_node_to_blocks[node]...) for node in nodes]
+    return [reduce(vcat, state.evaluated_node_to_blocks[node]) for node in nodes]
 end
 
 function evaluate(
