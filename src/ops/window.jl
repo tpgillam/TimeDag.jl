d052c732220cf0eb9c18e05176e87607ca21f8f1
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2021-10-08 22:37:38 +0100

    Make _combine part of the API for window ops

--- src/ops/window.jl
+++ src/ops/window.jl
@@ -21,6 +21,14 @@ The return value determines whether a knot should be emitted for this value.
 function _should_tick end
 
 """
+    _combine(op, data_1, data_2) -> Data
+
+This should be defined for all inception and windowed ops. Given two data objects, combine
+them into a new data object.
+"""
+function _combine end
+
+"""
     _extract(op, data) -> value
 
 This should be defined for all inception and windowed ops. Given some data object, it should
@@ -29,14 +37,12 @@ compute the appropriate output value for the node.
 function _extract end
 
 """Unary operator accumulated from inception."""
-abstract type UnaryInceptionOp{T,Data,CombineOp} <: UnaryNodeOp{T} end
+abstract type UnaryInceptionOp{T,Data} <: UnaryNodeOp{T} end
 
 """Binary operator accumulated from inception."""
-abstract type BinaryInceptionOp{T,Data,CombineOp,A} <: BinaryAlignedNodeOp{T,A} end
+abstract type BinaryInceptionOp{T,Data,A} <: BinaryAlignedNodeOp{T,A} end
 
-const InceptionOp{T,Data,CombineOp} = Union{
-    UnaryInceptionOp{T,Data,CombineOp},BinaryInceptionOp{T,Data,CombineOp}
-}
+const InceptionOp{T,Data} = Union{UnaryInceptionOp{T,Data},BinaryInceptionOp{T,Data}}
 
 always_ticks(op::InceptionOp) = _unfiltered(op)
 time_agnostic(::InceptionOp) = true
@@ -61,13 +67,13 @@ function create_operator_evaluation_state(
 end
 
 function operator!(
-    op::InceptionOp{T,Data,CombineOp}, state::InceptionOpState{Data}, x...
-) where {T,Data,CombineOp}
+    op::InceptionOp{T,Data}, state::InceptionOpState{Data}, x...
+) where {T,Data}
     if !state.initialised
         state.data = _wrap(Data, x...)
         state.initialised = true
     else
-        state.data = CombineOp(state.data, _wrap(Data, x...))
+        state.data = _combine(op, state.data, _wrap(Data, x...))
     end
     return if always_ticks(op)
         # Deal with the case where we always emit.
@@ -82,12 +88,12 @@ end
 """
 Windowed associative binary operator, potentially emitting early before the window is full.
 """
-abstract type UnaryWindowOp{T,Data,CombineOp,EmitEarly} <: UnaryNodeOp{T} end
+abstract type UnaryWindowOp{T,Data,EmitEarly} <: UnaryNodeOp{T} end
 
-abstract type BinaryWindowOp{T,Data,CombineOp,EmitEarly,A} <: BinaryAlignedNodeOp{T,A} end
+abstract type BinaryWindowOp{T,Data,EmitEarly,A} <: BinaryAlignedNodeOp{T,A} end
 
-const WindowOp{T,Data,CombineOp,EmitEarly} = Union{
-    UnaryWindowOp{T,Data,CombineOp,EmitEarly},BinaryWindowOp{T,Data,CombineOp,EmitEarly}
+const WindowOp{T,Data,EmitEarly} = Union{
+    UnaryWindowOp{T,Data,EmitEarly},BinaryWindowOp{T,Data,EmitEarly}
 }
 
 """
@@ -99,10 +105,10 @@ The default implementation expects a field called `window` on the op structure.
 _window(op::WindowOp) = op.window
 
 """Whether or not this window op is set to emit with a non-full window."""
-function _emit_early(::WindowOp{T,Data,CombineOp,true}) where {T,Data,CombineOp}
+function _emit_early(::WindowOp{T,Data,true}) where {T,Data}
     return true
 end
-function _emit_early(::WindowOp{T,Data,CombineOp,false}) where {T,Data,CombineOp}
+function _emit_early(::WindowOp{T,Data,false}) where {T,Data}
     return false
 end
 
@@ -121,15 +127,13 @@ function create_operator_evaluation_state(::Tuple{Node,Node}, op::BinaryWindowOp
     return create_operator_evaluation_state(op)
 end
 
-function create_operator_evaluation_state(
-    op::WindowOp{T,Data,CombineOp}
-) where {T,Data,CombineOp}
-    return WindowOpState{Data}(FixedWindowAssociativeOp{Data,CombineOp}(_window(op)))
+function create_operator_evaluation_state(op::WindowOp{T,Data}) where {T,Data}
+    return WindowOpState{Data}(
+        FixedWindowAssociativeOp{Data,(x, y) -> _combine(op, x, y)}(_window(op))
+    )
 end
 
-function operator!(
-    op::WindowOp{T,Data,CombineOp}, state::WindowOpState{Data}, x...
-) where {T,Data,CombineOp}
+function operator!(op::WindowOp{T,Data}, state::WindowOpState{Data}, x...) where {T,Data}
     update_state!(state.window_state, _wrap(Data, x...))
     if always_ticks(op)
         # Deal with the case where we always emit.
@@ -150,8 +154,9 @@ function operator!(
 end
 
 # Sum, cumulative over time.
-struct Sum{T} <: UnaryInceptionOp{T,T,+} end
+struct Sum{T} <: UnaryInceptionOp{T,T} end
 _unfiltered(::Sum) = true
+_combine(::Sum, x, y) = x + y
 _extract(::Sum, data) = data
 Base.show(io::IO, ::Sum{T}) where {T} = print(io, "Sum{$T}")
 function Base.sum(x::Node)
@@ -160,10 +165,11 @@ function Base.sum(x::Node)
 end
 
 # Sum over fixed window.
-struct WindowSum{T,EmitEarly} <: UnaryWindowOp{T,T,+,EmitEarly}
+struct WindowSum{T,EmitEarly} <: UnaryWindowOp{T,T,EmitEarly}
     window::Int64
 end
 _unfiltered(::WindowSum) = true
+_combine(::WindowSum, x, y) = x + y
 _extract(::WindowSum, data) = data
 Base.show(io::IO, op::WindowSum{T}) where {T} = print(io, "WindowSum{$T}($(_window(op)))")
 function Base.sum(x::Node, window::Int; emit_early::Bool=false)
@@ -171,8 +177,9 @@ function Base.sum(x::Node, window::Int; emit_early::Bool=false)
 end
 
 # Product, cumulative over time.
-struct Prod{T} <: UnaryInceptionOp{T,T,*} end
+struct Prod{T} <: UnaryInceptionOp{T,T} end
 _unfiltered(::Prod) = true
+_combine(::Prod, x, y) = x * y
 _extract(::Prod, data) = data
 Base.show(io::IO, ::Prod{T}) where {T} = print(io, "Prod{$T}")
 function Base.prod(x::Node)
@@ -181,10 +188,11 @@ function Base.prod(x::Node)
 end
 
 # Product over fixed window.
-struct WindowProd{T,EmitEarly} <: UnaryWindowOp{T,T,*,EmitEarly}
+struct WindowProd{T,EmitEarly} <: UnaryWindowOp{T,T,EmitEarly}
     window::Int64
 end
 _unfiltered(::WindowProd) = true
+_combine(::WindowProd, x, y) = x * y
 _extract(::WindowProd, data) = data
 Base.show(io::IO, op::WindowProd{T}) where {T} = print(io, "WindowProd{$T}($(_window(op)))")
 function Base.prod(x::Node, window::Int; emit_early::Bool=false)
@@ -198,14 +206,15 @@ end
 const MeanData{T} = @NamedTuple{n::Int64, mean::T} where {T}
 #! format: on
 _wrap(::Type{MeanData{T}}, x) where {T} = MeanData{T}((1, x))
-function _combine(state_a::MeanData{T}, state_b::MeanData{T})::MeanData{T} where {T}
+struct Mean{T} <: UnaryInceptionOp{T,MeanData{T}} end
+_unfiltered(::Mean) = true
+function _combine(state_a::MeanData{T}, state_b::MeanData{T}) where {T}
     na = state_a.n
     nb = state_b.n
     nc = na + nb
     return MeanData{T}((n=nc, mean=state_a.mean * (na / nc) + state_b.mean * (nb / nc)))
 end
-struct Mean{T} <: UnaryInceptionOp{T,MeanData{T},_combine} end
-_unfiltered(::Mean) = true
+_combine(::Mean, x::MeanData, y::MeanData) = _combine(x, y)
 _extract(::Mean, data::MeanData) = data.mean
 Base.show(io::IO, ::Mean{T}) where {T} = print(io, "Mean{$T}")
 function Statistics.mean(x::Node)
@@ -215,10 +224,11 @@ function Statistics.mean(x::Node)
 end
 
 # Mean over fixed window.
-struct WindowMean{T,EmitEarly} <: UnaryWindowOp{T,MeanData{T},_combine,EmitEarly}
+struct WindowMean{T,EmitEarly} <: UnaryWindowOp{T,MeanData{T},EmitEarly}
     window::Int64
 end
 _unfiltered(::WindowMean) = true
+_combine(::WindowMean, x::MeanData, y::MeanData) = _combine(x, y)
 _extract(::WindowMean, data::MeanData) = data.mean
 Base.show(io::IO, op::WindowMean{T}) where {T} = print(io, "WindowMean{$T}($(_window(op)))")
 function Statistics.mean(x::Node, window::Int; emit_early::Bool=false)
@@ -233,7 +243,9 @@ end
 const VarData{T} = @NamedTuple{n::Int64, mean::T, s::T} where {T}
 #! format: on
 _wrap(::Type{VarData{T}}, x) where {T} = VarData{T}((1, x, 0))
-function _combine(state_a::VarData{T}, state_b::VarData{T})::VarData{T} where {T}
+struct Var{T,corrected} <: UnaryInceptionOp{T,VarData{T}} end
+_should_tick(::Var, data::VarData) = data.n > 1
+function _combine(state_a::VarData{T}, state_b::VarData{T}) where {T}
     na = state_a.n
     nb = state_b.n
     nc = na + nb
@@ -247,8 +259,7 @@ function _combine(state_a::VarData{T}, state_b::VarData{T})::VarData{T} where {T
 
     return VarData{T}((n=nc, mean=μc, s=(sa + sb) + nb * (μb - μa) * (μb - μc)))
 end
-struct Var{T,corrected} <: UnaryInceptionOp{T,VarData{T},_combine} end
-_should_tick(::Var, data::VarData) = data.n > 1
+_combine(::Var, x::VarData, y::VarData) = _combine(x, y)
 _extract(::Var{T,true}, data::VarData) where {T} = data.s / (data.n - 1)
 _extract(::Var{T,false}, data::VarData) where {T} = data.s / data.n
 Base.show(io::IO, ::Var{T}) where {T} = print(io, "Var{$T}")
@@ -259,10 +270,11 @@ function Statistics.var(x::Node; corrected::Bool=true)
 end
 
 # Variance over fixed window.
-struct WindowVar{T,Corrected,EmitEarly} <: UnaryWindowOp{T,VarData{T},_combine,EmitEarly}
+struct WindowVar{T,Corrected,EmitEarly} <: UnaryWindowOp{T,VarData{T},EmitEarly}
     window::Int64
 end
 _should_tick(::WindowVar, data::VarData) = data.n > 1
+_combine(::WindowVar, x::VarData, y::VarData) = _combine(x, y)
 _extract(::WindowVar{T,true}, data::VarData) where {T} = data.s / (data.n - 1)
 _extract(::WindowVar{T,false}, data::VarData) where {T} = data.s / data.n
 Base.show(io::IO, op::WindowVar{T}) where {T} = print(io, "WindowVar{$T}($(_window(op)))")
@@ -285,7 +297,9 @@ end
 const CovData{T} = @NamedTuple{n::Int64, mean_x::T, mean_y::T, c::T} where {T}
 #! format: on
 _wrap(::Type{CovData{T}}, x, y) where {T} = CovData{T}((1, x, y, 0))
-function _combine(state_a::CovData{T}, state_b::CovData{T})::CovData{T} where {T}
+struct Cov{T,corrected,A} <: BinaryInceptionOp{T,CovData{T},A} end
+_should_tick(::Cov, data::CovData) = data.n > 1
+function _combine(state_a::CovData{T}, state_b::CovData{T}) where {T}
     na = state_a.n
     nb = state_b.n
     nc = na + nb
@@ -305,11 +319,10 @@ function _combine(state_a::CovData{T}, state_b::CovData{T})::CovData{T} where {T
 
     return CovData{T}((n=nc, mean_x=μxc, mean_y=μyc, c=cc))
 end
-struct Cov{T,corrected,A} <: BinaryInceptionOp{T,CovData{T},_combine,A} end
-_should_tick(::Cov, data::CovData) = data.n > 1
+_combine(::Cov, x::CovData, y::CovData) = _combine(x, y)
 _extract(::Cov{T,true}, data::CovData) where {T} = data.c / (data.n - 1)
 _extract(::Cov{T,false}, data::CovData) where {T} = data.c / data.n
-Base.show(io::IO, ::Cov{T}) where {T} = print(io, "Var{$T}")
+Base.show(io::IO, ::Cov{T}) where {T} = print(io, "Cov{$T}")
 function Statistics.cov(x, y, ::A; corrected::Bool=true) where {A<:Alignment}
     x = _ensure_node(x)
     y = _ensure_node(y)
@@ -326,11 +339,11 @@ Statistics.cov(x::Node, y; corrected::Bool=true) = cov(x, y, DEFAULT_ALIGNMENT; 
 Statistics.cov(x, y::Node; corrected::Bool=true) = cov(x, y, DEFAULT_ALIGNMENT; corrected)
 
 # Covariance over fixed window.
-struct WindowCov{T,Corrected,EmitEarly,A} <:
-       BinaryWindowOp{T,CovData{T},_combine,EmitEarly,A}
+struct WindowCov{T,Corrected,EmitEarly,A} <: BinaryWindowOp{T,CovData{T},EmitEarly,A}
     window::Int64
 end
 _should_tick(::WindowCov, data::CovData) = data.n > 1
+_combine(::WindowCov, x::CovData, y::CovData) = _combine(x, y)
 _extract(::WindowCov{T,true}, data::CovData) where {T} = data.c / (data.n - 1)
 _extract(::WindowCov{T,false}, data::CovData) where {T} = data.c / data.n
 Base.show(io::IO, op::WindowCov{T}) where {T} = print(io, "WindowCov{$T}($(_window(op)))")
