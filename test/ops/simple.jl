6d0f7e9fa6e3d510e43e374e8784f086909692b7
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2021-10-27 13:25:04 +0100

    Export wrap & wrapb, which wrap functions (the latter with broadcasting semantics)

--- test/ops/simple.jl
+++ test/ops/simple.jl
@@ -1,34 +1,19 @@
 const _UNARY_STUFF = [
-    (:-, b1),
-    (:exp, b1),
-    (:log, b1),
-    (:log10, b1),
-    (:log2, b1),
-    (:sqrt, b1),
-    (:cbrt, b1),
-    (:inv, b1),
-    (:!, b_boolean),
+    (-, b1),
+    (exp, b1),
+    (log, b1),
+    (log10, b1),
+    (log2, b1),
+    (sqrt, b1),
+    (cbrt, b1),
+    (inv, b1),
+    (!, b_boolean),
 ]
 
 @testset "unary" begin
     for (f, block) in _UNARY_STUFF
         @testset "$f" begin
-            @eval begin
-                # Check basic evaluation.
-                value = first($(block).values)
-                node = block_node($block)
-                @test _eval($f(node)) == _mapvalues($f, $block)
-
-                # Check constant propagation.
-                @test $f(constant(value)) === constant($f(value))
-
-                # Two instances of the NodeOp instance should compare equal for equal
-                # type parameters.
-                T = typeof(value)
-                @test TimeDag.SimpleUnary{$f,T}() == TimeDag.SimpleUnary{$f,T}()
-                @test T != Float32
-                @test TimeDag.SimpleUnary{$f,Float32}() != TimeDag.SimpleUnary{$f,T}()
-            end
+            _test_unary_op(f, block)
         end
     end
 
@@ -39,31 +24,13 @@ const _UNARY_STUFF = [
     end
 end
 
-const _BINARY_FUNCTIONS = [:+, :-, :*, :/, :^, :min, :max, :>, :<, :>=, :<=]
+const _BINARY_FUNCTIONS = [+, -, *, /, ^, min, max, >, <, >=, <=]
 
 @testset "binary" begin
     for f in _BINARY_FUNCTIONS
         @testset "$f" begin
-            @eval begin
-                # Test evaluations with different alignments.
-                _test_binary_op($f)
-
-                # Test constant propagation.
-                value = 2.0  # valid for all ops we're currently testing.
-                @test $f(constant(value), constant(value)) === constant($f(value, value))
-                @test $f(value, constant(value)) === constant($f(value, value))
-                @test $f(constant(value), value) === constant($f(value, value))
-
-                # Two instances of the NodeOp instance should compare equal for equal
-                # type parameters.
-                T = typeof(value)
-                for A in (UnionAlignment, IntersectAlignment, LeftAlignment)
-                    @test TimeDag.SimpleBinary{$f,T,A}() == TimeDag.SimpleBinary{$f,T,A}()
-                    @test T != Float32
-                    @test TimeDag.SimpleBinary{$f,Float32,A}() !=
-                        TimeDag.SimpleBinary{$f,T,A}()
-                end
-            end
+            # Test evaluations with different alignments.
+            _test_binary_op(f)
         end
     end
 end
