63f6a0f0fd06e52db6106b0296dfe340d8f16111
Tom Gillam <tpgillam@googlemail.com>
2022-01-06 22:34:55 +0000

    Improve documentation

--- src/ops/window.jl
+++ src/ops/window.jl
@@ -746,8 +746,20 @@ The decay is specified either by `α`, which should satisfy `0 < α < 1`, or by 
 should be an integer greater than 1. If the latter is specified, then we compute
 `α = 2 / (w_eff + 1)`.
 
-See e.g. the conventions here:
-https://en.wikipedia.org/wiki/Moving_average#Exponential_moving_average
+For internal state ``s_t``, with ``s_0 = 0``, and resulting EMA series ``m_t``, this has the
+form:
+
+```math
+\\begin{aligned}
+s_t &= s_{t-1} + (1 - \\alpha) x_t \\\\
+m_t &= \\frac{\\alpha s_t}{1 - (1 - \\alpha)^t}.
+\\end{aligned}
+```
+
+For further information, see the notational conventions and discussion on
+(Wikipedia)[https://en.wikipedia.org/wiki/Moving_average#Exponential_moving_average]. Note
+that this function implements the variant including the correction for the initial
+convergence problem.
 """
 function ema(x::Node, α::AbstractFloat)
     T = output_type(*, value_type(x), Float64)
