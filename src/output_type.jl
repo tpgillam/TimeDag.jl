8550cee1c9b2f3993459df6d2fb732c157b17428
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2022-02-25 14:21:17 +0000

    Remove special-casing from output_type. Sometimes it was wrong.

--- src/output_type.jl
+++ src/output_type.jl
@@ -12,17 +12,3 @@ Return the output type of the specified function. Tries to be fast where possibl
     of the output of a node without yet knowing the concrete values of the input type.
 """
 output_type(f, arg_types...) = Base.promote_op(f, arg_types...)
-
-output_type(::typeof(-), x) = x
-# TODO more efficient versions for common exp & log cases
-
-output_type(::typeof(+), x, y) = promote_type(x, y)
-output_type(::typeof(-), x, y) = promote_type(x, y)
-output_type(::typeof(*), x, y) = promote_type(x, y)
-
-output_type(::typeof(/), ::Type{T}, ::Type{T}) where {T<:AbstractFloat} = T
-function output_type(::typeof(/), ::Type{X}, ::Type{Y}) where {X<:Real,Y<:Real}
-    T = promote_type(X, Y)
-    return output_type(/, T, T)
-end
-output_type(::typeof(/), ::Type{T}, ::Type{T}) where {T<:Integer} = Float64
