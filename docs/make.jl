5447a0d5d5bf6dda4f3277857ae7f97b5c3cdd53
Tom Gillam <tpgillam@googlemail.com>
2022-07-29 09:47:47 +0100

    Updates for repo move

--- docs/make.jl
+++ docs/make.jl
@@ -13,12 +13,12 @@ DocMeta.setdocmeta!(TimeDag, :DocTestSetup, :(using TimeDag); recursive=true)
 withenv("GKSwstype" => "nul") do
     makedocs(;
         modules=[TimeDag],
-        authors="Invenia Technical Computing Corporation",
-        repo="https://github.com/invenia/TimeDag.jl/blob/{commit}{path}#{line}",
+        authors="Invenia Technical Computing Corporation, Tom Gillam",
+        repo="https://github.com/tpgillam/TimeDag.jl/blob/{commit}{path}#{line}",
         sitename="TimeDag.jl",
         format=Documenter.HTML(;
             prettyurls=get(ENV, "CI", "false") == "true",
-            canonical="https://invenia.github.io/TimeDag.jl",
+            canonical="https://tpgillam.github.io/TimeDag.jl",
             assets=String[],
         ),
         pages=[
@@ -43,4 +43,4 @@ withenv("GKSwstype" => "nul") do
     )
 end
 
-deploydocs(; repo="github.com/invenia/TimeDag.jl", devbranch="main")
+deploydocs(; repo="github.com/tpgillam/TimeDag.jl", devbranch="main")
