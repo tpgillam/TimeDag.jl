fd1fec021f3ea5bdbedc5591c38734d92c7947e5
Will Tebbutt <will.tebbutt@invenialabs.co.uk>
2022-08-04 10:01:29 +0100

    improve phrasing

--- src/core.jl
+++ src/core.jl
@@ -109,8 +109,8 @@ We return a new [`Block`](@ref) of output knots from this node.
         `[time_start, time_end)`.
     * **Consistency**: Calling `run_node!` over a single interval should give the same
         result as calling it multiple times over a decomposition of that same interval.
-        This is to ensure [`evaluate`](@ref) operates as intended when the `batch_interval`
-        kwarg is provided.
+        This is to ensures that the value returned by [`evaluate`](@ref) is invariant to the
+        `batch_interval` kwarg provided.
     * **Determinism**: `run_node!` should *always* be fully deterministic. If a
         pseudo-random number generator is required, it should be held on the evaluation
         state.
