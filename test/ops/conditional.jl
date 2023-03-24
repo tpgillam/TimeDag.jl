5305d3eeb904cdb7dd754140a7aa18226c26abde
Tom Gillam <tpgillam@googlemail.com>
2023-03-24 21:51:50 +0000

    More thorough testing

--- test/ops/conditional.jl
+++ test/ops/conditional.jl
@@ -65,6 +65,17 @@ end
             DateTime(2000, 1, 3) => 3,
             DateTime(2000, 1, 4) => 4
         ])
+
+    @test _eval(zap_until(n_boolean, DateTime(2000, 1, 3))) ==
+        Block([
+            DateTime(2000, 1, 3) => true,
+            DateTime(2000, 1, 4) => true
+        ])
+
+    for cutoff in first(b4.times):Minute(133):last(b4.times)
+        @test _eval(zap_until(n4, cutoff)) ==
+            TimeDag._slice(b4, cutoff, last(b4.times) + Hour(1))
+    end
 end
 
 #! format: on
